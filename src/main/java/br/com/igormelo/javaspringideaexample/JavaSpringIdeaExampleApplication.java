4cf3a7f960c8bfcbc58b8a2dfaf86e10f5c2fcdc
Igor Melo <igorgalvao@gec.inatel.br>
2021-01-15 14:00:27 -0300

    Projeto Criado

---
+++ src/main/java/br/com/igormelo/javaspringideaexample/JavaSpringIdeaExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.igormelo.javaspringideaexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringIdeaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringIdeaExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/igormelo/javaspringideaexample/JavaSpringIdeaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.igormelo.javaspringideaexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringIdeaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
