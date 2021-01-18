ec01d4f6b6216d942c4cb9a45ef1432358910345
Igor Melo <igorgalvao@gec.inatel.br>
2021-01-18 11:47:44 -0300

    Service layer, component registration

--- src/main/java/br/com/igormelo/cursojava/entities/User.java
+++ src/main/java/br/com/igormelo/cursojava/entities/User.java
@@ -18,6 +18,8 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
+    public User(){}
+
     public User(Long id, String name, String email, String phone, String password) {
         this.id = id;
         this.name = name;
---
+++ src/main/java/br/com/igormelo/cursojava/services/UserService.java
@@ -0,0 +1,25 @@
+package br.com.igormelo.cursojava.services;
+
+import br.com.igormelo.cursojava.entities.User;
+import br.com.igormelo.cursojava.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = userRepository.findById(id);
+        return obj.get();
+    }
+}
